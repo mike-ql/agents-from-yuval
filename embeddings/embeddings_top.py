6250440973ee00ae599ed56c6b6e177f36a2a078
Yuval SHaul <yuval.shaul@gmail.com>
2025-08-20 11:49:18 +0300

    updating emmbedings

--- embeddings/embeddings_top.py
+++ embeddings/embeddings_top.py
@@ -1,3 +1,29 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 import extract_pdf
 
-doc_texts = extract_pdf.extract_all_docs()
+
+def prepare_agent():
+    doc_texts = extract_pdf.extract_all_docs()
+    all_chunks = make_chunks(doc_texts)
+    print(f"Created {len(all_chunks)} chunks")
+    print(all_chunks[0])
+
+
+
+def make_chunks(list_of_texts):
+    # Simple chunking
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200
+    )
+
+    # Split all documents
+    all_chunks = []
+    for text in list_of_texts:
+        chunks = text_splitter.split_text(text)
+        all_chunks.extend(chunks)
+
+    return all_chunks
+
+
+prepare_agent()
